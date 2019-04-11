f1a0dce5c3375c80e18f6c7e551fb93b385ba5da
pheeria <olzhas.askar@gmail.com>
2019-04-11 19:21:01 +0200

    Add google maps

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react';
 import Geosuggest from 'react-geosuggest';
 import './geosuggest.css';
+import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+
+const berlin = {
+  lat: 52.52000659999999,
+  lng: 13.404953999999975
+};
+
+const MyMapComponent = withGoogleMap(props => (
+  <GoogleMap
+    defaultZoom={8}
+    defaultCenter={{ lat: berlin.lat, lng: berlin.lng }}
+  >
+    {props.isMarkerShown && (
+      <Marker position={{ lat: 52.5248, lng: 13.393141600000035 }} />
+    )}
+  </GoogleMap>
+));
 
 class App extends Component {
   onSuggestSelect(suggest) {
@@ -15,9 +32,15 @@ class App extends Component {
           onSuggestSelect={this.onSuggestSelect}
           location={
             // eslint-disable-next-line
-            new google.maps.LatLng(52.52000659999999, 13.404953999999975)
+            new google.maps.LatLng(berlin.lat, berlin.lng)
           }
           radius="20"
+        />
+        <MyMapComponent
+          isMarkerShown
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `400px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
         />
       </div>
     );
