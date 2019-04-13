d6dc84d13aec7696d5eb07ff14e5cfae4dfb1e9c
pheeria <olzhas.askar@gmail.com>
2019-04-13 14:00:37 +0200

    Add sorting

--- src/app/App.js
+++ src/app/App.js
@@ -1,33 +1,29 @@
-import React, { Component } from 'react';
-import ListContainer from '../list/ListContainer';
-import './App.css';
+import React from 'react';
 import { Layout } from 'antd';
 import SuggestContainer from '../suggest/SuggestContainer';
 import MapContainer from '../map/MapContainer';
+import ListContainer from '../list/ListContainer';
+import './App.css';
 
 const { Header, Footer, Sider, Content } = Layout;
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Layout>
-          <Header style={{ height: `15vh`, backgroundColor: `white` }}>
-            <SuggestContainer />
-          </Header>
-          <Layout>
-            <Content>
-              <MapContainer />
-            </Content>
-            <Sider width={`400px`} style={{ backgroundColor: `white` }}>
-              <ListContainer />
-            </Sider>
-          </Layout>
-          <Footer>Footer</Footer>
-        </Layout>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <Layout>
+      <Header style={{ height: `15vh`, backgroundColor: `white` }}>
+        <SuggestContainer />
+      </Header>
+      <Layout>
+        <Content>
+          <MapContainer />
+        </Content>
+        <Sider width={`300px`} style={{ backgroundColor: `white` }}>
+          <ListContainer />
+        </Sider>
+      </Layout>
+      <Footer>Footer</Footer>
+    </Layout>
+  </div>
+);
 
 export default App;
--- src/list/List.js
+++ src/list/List.js
@@ -1,36 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { List as AntList, Rate, Icon } from 'antd';
 
-const List = ({ places, selectedPlace, removePlace }) => (
-  <AntList
-    itemLayout="horizontal"
-    dataSource={places}
-    renderItem={place => (
-      <AntList.Item
-        actions={[<Icon onClick={() => removePlace(place)} type="close" />]}
-        style={
-          selectedPlace && selectedPlace.id === place.id
-            ? { border: `1px solid red` }
-            : null
-        }
-      >
-        <AntList.Item.Meta
-          title={<p>{place.name}</p>}
-          description={
-            <div>
-              <div>
-                <Icon type="heart" theme="twoTone" twoToneColor="#eb2f96" />:{' '}
-                {place.favoriteFood}
-              </div>
-              <div>
-                <Rate disabled defaultValue={place.rating} />
-              </div>
-            </div>
-          }
-        />
-      </AntList.Item>
-    )}
-  />
-);
+const List = ({ places, selectedPlace, removePlace }) => {
+  const [sortAscending, setSortAscending] = useState(true);
+  const [sortedPlaces, setSortedPlaces] = useState(places);
+  return (
+    <div>
+      <Icon
+        type={sortAscending ? 'sort-ascending' : 'sort-descending'}
+        onClick={() => {
+          setSortAscending(!sortAscending);
+          setSortedPlaces(
+            sortedPlaces.sort((a, b) =>
+              sortAscending ? a.name < b.name : a.name > b.name
+            )
+          );
+        }}
+      />
+      <AntList
+        itemLayout="horizontal"
+        dataSource={sortedPlaces}
+        renderItem={place => (
+          <AntList.Item
+            actions={[<Icon onClick={() => removePlace(place)} type="close" />]}
+            style={
+              selectedPlace && selectedPlace.id === place.id
+                ? { border: `1px solid red` }
+                : null
+            }
+          >
+            <AntList.Item.Meta
+              title={<p>{place.name}</p>}
+              description={
+                <div>
+                  <div>
+                    <Icon type="heart" theme="twoTone" twoToneColor="#eb2f96" />
+                    : {place.favoriteFood}
+                  </div>
+                  <div>
+                    <Rate disabled defaultValue={place.rating} />
+                  </div>
+                </div>
+              }
+            />
+          </AntList.Item>
+        )}
+      />
+    </div>
+  );
+};
 
 export default List;
