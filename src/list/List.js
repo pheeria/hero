453c238f69fbd58c6b93290b9aa88e7b421275f6
pheeria <olzhas.askar@gmail.com>
2019-04-13 14:52:52 +0200

    Add sorting

--- src/list/List.js
+++ src/list/List.js
@@ -1,17 +1,30 @@
 import React, { useState } from 'react';
-import { List as AntList, Rate, Icon } from 'antd';
+import { List as AntList, Rate, Icon, AutoComplete } from 'antd';
 
 const List = ({ places, selectedPlace, removePlace }) => {
   const [sortAscending, setSortAscending] = useState(true);
-  const [sortedPlaces, setSortedPlaces] = useState(places);
+  const [modifiedPlaces, setModifiedPlaces] = useState(places);
   return (
     <div>
+      <AutoComplete
+        dataSource={modifiedPlaces.map(mp => mp.name)}
+        style={{ width: `250px` }}
+        onSelect={p =>
+          setModifiedPlaces(places.filter(mp => mp.name.includes(p)))
+        }
+        onSearch={p =>
+          !p
+            ? setModifiedPlaces(places)
+            : setModifiedPlaces(places.filter(mp => mp.name.includes(p)))
+        }
+        placeholder="Search"
+      />
       <Icon
         type={sortAscending ? 'sort-ascending' : 'sort-descending'}
         onClick={() => {
           setSortAscending(!sortAscending);
-          setSortedPlaces(
-            sortedPlaces.sort((a, b) =>
+          setModifiedPlaces(
+            modifiedPlaces.sort((a, b) =>
               sortAscending ? a.name < b.name : a.name > b.name
             )
           );
@@ -19,7 +32,7 @@ const List = ({ places, selectedPlace, removePlace }) => {
       />
       <AntList
         itemLayout="horizontal"
-        dataSource={sortedPlaces}
+        dataSource={modifiedPlaces}
         renderItem={place => (
           <AntList.Item
             actions={[<Icon onClick={() => removePlace(place)} type="close" />]}
